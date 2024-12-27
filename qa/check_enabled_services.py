a7913279778966ee75ee8c616c8bd0d5e368874b
Chris Niessl <niessl@hellbender.com>
2024-12-27 13:21:03 -0500

    Add in LTE check

--- qa/check_enabled_services.py
+++ qa/check_enabled_services.py
@@ -1,4 +1,12 @@
+
+import time
 import subprocess
+
+def reenable_and_test_LTE():
+  subprocess.run(["systemctl", "enable", "lte"])
+  time.sleep(3)
+  subprocess.run(["minicom", "-D", "/dev/ttyUSB2", "-S", "check_lte_script.txt", "-C", "lte_capture.txt"])
+  subprocess.run(["cat", "lte_capture.txt"])
 
 def get_enabled_services():
     """Gets a list of enabled systemctl services."""
@@ -39,4 +47,5 @@ def check_enabled_services():
             print(f"[PASS] Service {service} is enabled.")
 
 if __name__ == "__main__":
+    reenable_and_test_LTE()
     check_enabled_services()
