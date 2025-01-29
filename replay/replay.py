2ea75db7a5854dc8026c6421cc7066e33eba0643
derekhive <derek@hivemapper.com>
2025-01-28 17:37:28 -0800

    start of sensor replay

---
+++ replay/replay.py
@@ -0,0 +1,211 @@
+"""Simulator to replay Bee data.
+
+Setup:
+1. pip install redis pandas protobuf
+2. protoc --python_out=. sensordata.proto
+
+"""
+
+import time
+import sqlite3
+import argparse
+import subprocess
+
+import redis
+import pandas as pd
+
+import sensordata_pb2 as sensordata
+
+
+def main():
+    """Main function to fetch, serialize, and push data."""
+
+    # get path to database from argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--db_path", type=str, default="sensors-v0-0-2.db", help="Path to the SQLite database file")
+    args = parser.parse_args()
+
+    sr = SensorReplay(args.db_path)
+    sr.run_replay()
+
+class SensorReplay():
+    def __init__(self, sensor_db_path):
+
+
+        # Redis configuration
+        self.redis_host = "127.0.0.1"
+        self.redis_port = 6379
+        self.redis_conf_file = "redis.conf"
+        self.sensor_db_path = sensor_db_path
+
+        
+        self.redis_table_to_list = {
+                                "gnss" : "gnss_data",
+                                "gnss_auth" : "gnss_auth_data",
+                                "imu" : "imu_data",
+                                "magnetometer" : "magnetometer_data",
+                                "nav_cov" : "NavCov",
+                                "nav_posecef" : "NavPosecef",
+                                "nav_pvt" : "NavPvt",
+                                "nav_status" : "NavStatus",
+                                "nav_timegps" : "NavTimegps",
+                                "nav_velecef" : "NavVelecef",
+                              }
+        
+        self.serializers = {
+                                "gnss" : self.serialize_gnss,
+                                "gnss_auth" : self.serialize_gnss_auth,
+                                "imu" : self.serialize_imu,
+                                "magnetometer" : self.serialize_mag,
+                                "nav_cov" : self.serialize_nav_cov,
+                                "nav_posecef" : self.serialize_nav_posecef,
+                                "nav_pvt" : self.serialize_nav_pvt,
+                                "nav_status" : self.serialize_nav_status,
+                                "nav_timegps" : self.serialize_nav_timegps,
+                                "nav_velecef" : self.serialize_nav_velecef,
+                            }
+        
+
+        self.redis_unsaved_lists = ["NavDop","MonRf"]
+
+        self.system_time_columns = {
+                                "gnss" : "system_time",
+                                "gnss_auth" : "system_time",
+                                "imu" : "time",
+                                "magnetometer" : "system_time",
+                                "nav_pvt" : "system_time",
+                              }
+        self.itow_ms_tables = ["nav_cov", "nav_posecef", "nav_status", "nav_timegps", "nav_velecef"]
+        
+        self.sql_data = {}
+        self.row_index = {}
+        self.system_timestamps = {}
+        for table in self.redis_table_to_list:
+            self.sql_data[table] = self.fetch_sqlite_table(table)
+            self.row_index[table] = 0
+            if table in self.system_time_columns:
+                self.sql_data[table][self.system_time_columns[table]] = pd.to_datetime(self.sql_data[table][self.system_time_columns[table]], format="mixed")
+                if len(self.sql_data[table]) > 0:
+                    self.system_timestamps[table] = self.sql_data[table][self.system_time_columns[table]][0]
+
+        self.start_redis_server()
+
+    def run_replay(self):
+        """Runs the replay loop."""
+        
+        while True:
+
+            # find the minimum timestamp
+            valid_timestamps = {k: v for k, v in self.system_timestamps.items() if v is not None}
+            if len(valid_timestamps) == 0:
+                print("No more data to replay.")
+                break
+            min_key = min(valid_timestamps, key=valid_timestamps.get)
+            
+            # add the current min key to Redis
+            serialized_data = self.serializers[min_key](self.sql_data[min_key].iloc[self.row_index[min_key]])
+            self.push_to_redis(serialized_data, self.redis_table_to_list[min_key])
+            
+            # if it's a navigation message, also add the other navigation messages
+            if min_key == "nav_pvt":
+                for table in self.itow_ms_tables:
+                    data_row = self.sql_data[table].loc[self.sql_data[table]["itow_ms"] == self.sql_data[min_key].iloc[self.row_index[min_key]]["itow_ms"]]
+                    if len(data_row) > 0:
+                        serialized_data = self.serializers[table](data_row.iloc[0])
+                        self.push_to_redis(serialized_data, self.redis_table_to_list[table])
+            
+            # update to the next timestamp
+            self.row_index[min_key] += 1
+            if self.row_index[min_key] >= len(self.sql_data[min_key]):
+                self.system_timestamps[min_key] = None
+            else:
+                self.system_timestamps[min_key] = self.sql_data[min_key][self.system_time_columns[min_key]][self.row_index[min_key]]
+
+    def serialize_gnss(self, row):
+        message = sensordata.GnssData()
+        return message.SerializeToString()
+    
+    def serialize_gnss_auth(self, row):
+        message = sensordata.GnssData()
+        return message.SerializeToString()
+
+    def serialize_imu(self, row):
+        message = sensordata.ImuData()
+        message.time = row.time.strftime('%Y-%m-%d %H:%M:%S.') + str(row.time.microsecond).ljust(6, '0')
+        message.accelerometer.x = row.acc_x
+        message.accelerometer.y = row.acc_y
+        message.accelerometer.z = row.acc_z
+        message.gyroscope.x = row.gyro_x
+        message.gyroscope.y = row.gyro_y
+        message.gyroscope.z = row.gyro_z
+        message.temperature = row.temperature
+        return message.SerializeToString()
+    
+    def serialize_mag(self, row):
+        message = sensordata.MagnetometerData()
+        message.system_time = row.system_time.strftime('%Y-%m-%d %H:%M:%S.') + str(row.system_time.microsecond).ljust(6, '0')
+        message.x = row.mag_x
+        message.y = row.mag_y
+        message.z = row.mag_z
+        return message.SerializeToString()
+    
+    def serialize_nav_cov(self, row):
+        message = sensordata.NavCov()
+        return message.SerializeToString()
+    
+    def serialize_nav_posecef(self, row):
+        message = sensordata.NavPosecef()
+        return message.SerializeToString()
+    
+    def serialize_nav_pvt(self, row):
+        message = sensordata.NavPvt()
+        return message.SerializeToString()
+    
+    def serialize_nav_status(self, row):
+        message = sensordata.NavStatus()
+        return message.SerializeToString()
+    
+    def serialize_nav_timegps(self, row):
+        message = sensordata.NavTimegps()
+        return message.SerializeToString()
+    
+    def serialize_nav_velecef(self, row):
+        message = sensordata.NavVelecef()
+        return message.SerializeToString()
+
+    def push_to_redis(self, serialized_data, list_name):
+        """Pushes the serialized data to a Redis list."""
+        redis_client = redis.StrictRedis(host=self.redis_host, port=self.redis_port, decode_responses=True)
+        redis_client.lpush(list_name, serialized_data)
+
+    def fetch_sqlite_table(self, table_name):
+
+        conn = sqlite3.connect(self.sensor_db_path)
+        cursor = conn.cursor()
+
+        query = f"SELECT * FROM {table_name}"
+
+        df = pd.read_sql_query(query, conn)
+
+        conn.close()
+        return df
+
+    def start_redis_server(self):
+        """Starts a Redis server as a subprocess."""
+        try:
+            if self.redis_conf_file:
+                process = subprocess.Popen(["redis-server", self.redis_conf_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            else:
+                process = subprocess.Popen(["redis-server"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            # Wait a bit to ensure the Redis server starts
+            time.sleep(2)
+
+            print("Redis server started successfully.")
+            return process
+        except Exception as e:
+            print(f"Failed to start Redis server: {e}")
+            raise e
+
+if __name__ == "__main__":
+    main()
